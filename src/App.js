fc144088088f0fe7c447ac666eefa5c0f5aeb60f
Vigneshwaran Kannan <vkannan@quotient.com>
2021-09-19 22:40:23 +0530

    initial code setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        GeekTrust Frontend Challenge | Dashboard
       </header>
+      <section className="section">
+        <SearchBar />
+        {JSON.stringify(userData)}
+      </section>
+      <footer>
+        Creator: Vigneshwaran Kannan | kvigneshwaranit@gmail.com
+      </footer>
     </div>
   );
 }
